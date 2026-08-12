75f6db00c3a2c67399875c35d465534f7a81feb6
Viktor Smari <viktorsmari@gmail.com>
2026-08-12 13:56:51 +0000

    Dark mode toggle

--- toggle-dark-mode.sh
+++ toggle-dark-mode.sh
@@ -5,11 +5,11 @@ set -euo pipefail
 current=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$current" == "'prefer-dark'" ]]; then
-  gsettings set org.gnome.desktop.interface color-scheme default
   gsettings set org.gnome.desktop.interface gtk-theme Yaru
+  gsettings set org.gnome.desktop.interface color-scheme prefer-light
   echo "Switched to light mode"
 else
-  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   gsettings set org.gnome.desktop.interface gtk-theme Yaru-dark
+  gsettings set org.gnome.desktop.interface color-scheme prefer-dark
   echo "Switched to dark mode"
 fi
